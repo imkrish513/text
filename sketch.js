ce2d47d225b385df946bf23ed8f59b3c2e6519af
imkrish513 <70813672+imkrish513@users.noreply.github.com>
2020-11-28 11:57:50 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+var database;
+var form,game,player;
+var gameState = 0;
+var playerCount = 0;
+var backgroundIMG;
+function setup(){
+    database = firebase.database()
+    createCanvas(500,500);
+game = new Game()
+game.getState()
+game.start()
+}
+
+function draw(){
+    background("white");
+
+    drawSprites();
+}
